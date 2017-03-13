f43e8dbc49ec446dbd79c0102b64feabb89febd5
Joshua Tran <jtran6@ggc.edu>
2017-03-12 22:32:18 -0500

    created page

---
+++ 01_Hello_World/script1.js
@@ -0,0 +1,24 @@
+function horsemanOfWar()
+{
+  document.getElementById('heading2').innerHTML = "Horseman of War";
+  document.getElementById('godName').innerHTML = "Nemain";
+  document.getElementById('content').innerHTML = "The son of Uion was created from the tales of his Uion. He warps those into a panic and fear. He is considered more of a demon in the eyes as he is dark and is the more horrible side of war. Followers use dark magic to warp the fears of those in battle.";
+}
+function horsemanOfDeath()
+{
+  document.getElementById('heading2').innerHTML = "Horseman of Death";
+  document.getElementById('godName').innerHTML = "Macha";
+  document.getElementById('content').innerHTML = "Macha was born from Arawn’s presence as Arawn entered the otherworld. She takes those who have just died to her creator  Arawn. She has the ability to raise back the dead. She can not take anyone that has not been blessed properly. She knows the foribidden knowledge of necromancy.";
+}
+function horsemanOfPestilence()
+{
+  document.getElementById('heading2').innerHTML = "Horseman of Pestilence";
+  document.getElementById('godName').innerHTML = "Wenshen";
+  document.getElementById('content').innerHTML = "Many consider Wenshen a demon whose touch brings sickness to all. Nanshe came about along with his sister Limos. A hatred for their parent is noticed strongly and he wishes to destroy his mother’s creation.";
+}
+function horsemanOfFamine()
+{
+  document.getElementById('heading2').innerHTML = "Horseman of Famine";
+  document.getElementById('godName').innerHTML = "Limos";
+  document.getElementById('content').innerHTML = "Limos. The daughter of Nasha. When Nasha gave birth to one of the lesser gods.  Limos was born with the touch to kill plants unfortunately. Limos is almost always at odds with her mother.  She carries two scales in which she uses to destroy crops. ";
+}
